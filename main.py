bfa2caf5ed7072bca484d50cad4ea41fbb6b13e7
Merliananda <165452277+Merliananda@users.noreply.github.com>
2024-05-11 16:44:57 +0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,18 @@
+import streamlit as st
+st.title(':pink[🥳 SELAMAT ULANG TAHUN ILAA, YANG KE 23 🥳]')
+st.image('https://i.pinimg.com/originals/f2/32/f8/f232f8da4574ac4974849b32f36fa9b5.jpg')
+
+
+tombol=st.button('NIH TEKEN DULS')
+
+if tombol:
+        st.write('KEREN KAN GUA, SAPA DULU LIA')
+        st.image('https://img.buzzfeed.com/buzzfeed-static/static/2018-04/17/17/asset/buzzfeed-prod-web-02/sub-buzz-21544-1523999853-1.jpg?crop=675:825;0,255')
+        st.write('gacoan ya')
+tab1,tab2= st.tabs(["HARAPAN","DOA"])
+with tab1:
+        st.header('SEMOGA KAMU CEPET DAPET KERJA YA, BIAR BISA NGASIH DUIT KE GUA TIAP BULAN, GACOAN JUGA GPP')
+        st.image('https://i.pinimg.com/originals/5f/cd/43/5fcd430706e14f48244d2d790db441a9.jpg')
+with tab2:
+         st.header(':blue[DOANYA BIAR ALLAH AJA YANG TAHU 😈]')
+        
